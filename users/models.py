5c3e40ffb81af0574ba42346703320e410049b2b
BGOPC <bb8933400@gmail.com>
2023-03-12 20:56:31 +0330

    user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+from django.utils.text import slugify
+from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
+class User(AbstractUser):
+    username = models.SlugField(default="", null=False, db_index=True, blank=True)  # forced by django admin problems :(
+    password = models.CharField(max_length=255, null=True)
+    email = models.EmailField(max_length=255, unique=True)
+    phoneNum = PhoneNumberField(null=False, unique=True, default='')
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username", "first_name", "last_name", "password"]
+
+    def save(self, *args, **kwargs):
+        self.username = slugify(self.first_name + self.last_name)
+        super().save(*args, **kwargs)
