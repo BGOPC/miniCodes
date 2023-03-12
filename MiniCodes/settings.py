04c2020121c167c17da1ff94441772c453cd348d
BGOPC <bb8933400@gmail.com>
2023-03-12 20:55:46 +0330

    users app

--- MiniCodes/settings.py
+++ MiniCodes/settings.py
@@ -52,6 +52,10 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'MiniCodes.urls'
+# LOGIN_URL = 'login'
+# LOGIN_REDIRECT_URL = 'profile'
+# LOGOUT_REDIRECT_URL = 'login'
+# AUTH_USER_MODEL = 'users.User'
 
 TEMPLATES = [
     {
@@ -118,7 +122,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/')
+]
+MEDIA_ROOT = BASE_DIR / 'files'
+MEDIA_URL = 'files/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
