0dc3fc3008f83d32e44f5a0031c2e319f1a25c88
BGOPC <bb8933400@gmail.com>
2023-03-25 13:20:24 +0330

    Migrations, Codeisc.models Answer Model

--- codeisc/models.py
+++ codeisc/models.py
@@ -5,7 +5,8 @@ from users.models import User
 # Create your models here.
 class Code(models.Model):
     author = models.ForeignKey(User, on_delete=models.RESTRICT, related_name="Code_Author")
-    text = models.TextField(blank=True, null=False)
+    description = models.CharField(max_length=255, null=False, default="No Description Provided")
+    code_text = models.TextField(blank=True, null=False)
     type = models.CharField(max_length=3, choices=[
         ("TXT", "TextFile"),
         ("PY", "Python"),
@@ -21,7 +22,10 @@ class Code(models.Model):
 
 
 class Answer(models.Model):
-    pass
+    author = models.ForeignKey(User, on_delete=models.RESTRICT, related_name="Answer_Author")
+    text = models.TextField(null=False, blank=True)
+    code = models.ManyToManyField(Code)
+    score = models.IntegerField(default=0, null=False)
 
 
 class Question(models.Model):
