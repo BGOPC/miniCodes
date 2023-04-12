31e33f30162c57e9e974c6a42b3c49d06987ade7
BGOPC <bb8933400@gmail.com>
2023-04-12 16:38:14 +0330

    Typo, CodeView

--- codeisc/urls.py
+++ codeisc/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.HomeView.as_view(), name="home"),
+    path("codes/<int:codeID>", views.CodeView.as_view(), name="code")
 ]
--- codeisc/views.py
+++ codeisc/views.py
@@ -2,12 +2,24 @@ from django.shortcuts import render
 from django.views.generic import TemplateView, ListView, View
 from .models import Question, Answer, Code
 
+
 # Create your views here.
 
 class HomeView(TemplateView):
     template_name = 'codeisc/home.html'
 
-    def get_contex_data(self, **kwargs):
+    def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['user'] = self.request.user
+        return context
+
+
+class CodeView(TemplateView):
+    template_name = 'codeisc/codePage.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(** kwargs)
+        context['user'] = self.request.user
+        code_id = self.kwargs['codeID']
+        context['code'] = Code.objects.filter(id=code_id)
         return context
