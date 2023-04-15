9a7f9b460c44d5dfc43cca97f5173b3bc1281383
BGOPC <bb8933400@gmail.com>
2023-04-15 20:28:34 +0330

    urls for codeisc

--- codeisc/urls.py
+++ codeisc/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.HomeView.as_view(), name="home"),
-    path("codes/<int:codeID>", views.CodeView.as_view(), name="codePage"),
-    path("codes/<int:answerID>", views.AnswerView.as_view(), name="AnswerPage"),
-    path("codes/<int:questionID>", views.QuestionView.as_view(), name="QuestionPage")
+    path("codes/code/<int:codeID>", views.CodeView.as_view(), name="codePage"),
+    path("answers/answer/<int:answerID>", views.AnswerView.as_view(), name="AnswerPage"),
+    path("questions/question/<int:questionID>", views.QuestionView.as_view(), name="QuestionPage")
 ]
