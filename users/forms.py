37b01bf6c878d20d5ed49053e156fe3fd2066abc
BGOPC <bb8933400@gmail.com>
2023-04-26 19:24:00 +0330

    Changed forms for the profile pic

--- users/forms.py
+++ users/forms.py
@@ -1,29 +1,9 @@
 from django import forms
+from django.forms import ClearableFileInput
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from phonenumber_field.formfields import PhoneNumberField
 from phonenumber_field.widgets import PhoneNumberPrefixWidget
 from users.models import User
-
-
-class LoginForm(AuthenticationForm):
-
-    def __init__(self, request=None, *args, **kwargs):
-        super().__init__(request=None, *args, **kwargs)
-        self.fields['username'].label = 'email'
-        self.fields['username'].widget = forms.TextInput(attrs={
-            "class": "appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 "
-                     "mb-3 leading-tight focus:outline-none focus:bg-white text-center",
-            "placeholder": "email"
-        })
-        self.fields['password'].label = 'PassWord'
-        self.fields['password'].widget = forms.PasswordInput(attrs={'autocomplete': 'current-password',
-                                                                    "class": "appearance-none block w-full "
-                                                                             "bg-gray-200 text-gray-700 border "
-                                                                             "border-red-500 rounded py-3 px-4 mb-3 "
-                                                                             "leading-tight focus:outline-none "
-                                                                             "focus:bg-white text-center",
-                                                                    "placeholder": "Password"
-                                                                    })
 
 
 class NewUserForm(UserCreationForm):
@@ -40,6 +20,11 @@ class NewUserForm(UserCreationForm):
             "placeholder": "phone number"
         }
     ))
+    profile = forms.ImageField(required=False, widget=ClearableFileInput(attrs={
+        "class": "appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 "
+                 "mb-3 leading-tight focus:outline-none text-center focus:bg-white",
+        "placeholder": "profile picture"
+    }))
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -76,7 +61,7 @@ class NewUserForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ("first_name", "last_name", "email", "password1", "password2")
+        fields = ("first_name", "last_name", "email", "password1", "password2", "profile")
         help_texts = {
             'password1': """You password shouldn't be full numeric and should have at least 8 characters"""
         }
