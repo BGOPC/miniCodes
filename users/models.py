f1a5cdc0abf38fab14973b87ae0dec7aae5f92a7
BGOPC <bb8933400@gmail.com>
2023-06-17 18:13:49 +0330

    added default profile picture

--- users/models.py
+++ users/models.py
@@ -11,7 +11,8 @@ class User(AbstractUser):
     email = models.EmailField(max_length=255, unique=True)
     reputation = models.DecimalField(max_digits=7, decimal_places=1, default=1.0)
     phoneNum = PhoneNumberField(null=False, unique=True, default='')
-    profile = models.ImageField(upload_to="profile/", null=True, blank=True)
+    profile = models.ImageField(upload_to="profile/", null=True, blank=True,
+                                default="https://www.seekpng.com/png/detail/413-4139803_unknown-profile-profile-picture-unknown.png")
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username", "first_name", "last_name", "password"]
 
