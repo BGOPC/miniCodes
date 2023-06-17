49d1493b6703626b8765f900b2b4b1bb9a457cc3
BGOPC <bb8933400@gmail.com>
2023-06-17 18:13:02 +0330

    reverted the deletion of login form

--- users/forms.py
+++ users/forms.py
@@ -72,3 +72,24 @@ class NewUserForm(UserCreationForm):
         if commit:
             user.save()
         return user
+
+
+class LoginForm(AuthenticationForm):
+
+    def __init__(self, request=None, *args, **kwargs):
+        super().__init__(request=None, *args, **kwargs)
+        self.fields['username'].label = 'email'
+        self.fields['username'].widget = forms.TextInput(attrs={
+            "class": "appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 "
+                     "mb-3 leading-tight focus:outline-none focus:bg-white text-center",
+            "placeholder": "email"
+        })
+        self.fields['password'].label = 'PassWord'
+        self.fields['password'].widget = forms.PasswordInput(attrs={'autocomplete': 'current-password',
+                                                                    "class": "appearance-none block w-full "
+                                                                             "bg-gray-200 text-gray-700 border "
+                                                                             "border-red-500 rounded py-3 px-4 mb-3 "
+                                                                             "leading-tight focus:outline-none "
+                                                                             "focus:bg-white text-center",
+                                                                    "placeholder": "Password"
+                                                                    })
