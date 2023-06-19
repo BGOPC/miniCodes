491a969df93d172100f770382666578ee6b7eb5e
BGOPC <bb8933400@gmail.com>
2023-06-19 20:49:45 +0330

    Added URLs For new Question and Code
    Views And forms were ready somebody forgot to add the urls

--- codeisc/urls.py
+++ codeisc/urls.py
@@ -5,7 +5,9 @@ urlpatterns = [
     path("", views.HomeView.as_view(), name="home"),
     path("codes/<slug:username>", views.CodesListView.as_view(), name="UserCodes"),
     path("codes/code/<int:codeID>", views.CodeView.as_view(), name="CodePage"),
+    path("codes/create", views.CodeCreateView.as_view(), name="CreateCode"),
     path("questions/question/<int:questionID>", views.QuestionView.as_view(), name="QuestionPage"),
     path("questions/question/<int:questionID>/<int:answerID>", views.QuestionView.as_view(), name="AnswerPage"),
     path("questions/<slug:username>", views.QuestionsListView.as_view(), name="UserQuestions"),
+    path("questions/create", views.QuestionCreateView.as_view(), name="CreateQuestion")
 ]
